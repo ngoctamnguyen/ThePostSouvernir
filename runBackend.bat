e8e80f7931d4149ebb2b4ee10591a7faa8262dda
Tam Nguyen <tam505us@gmail.com>
2024-11-02 12:49:34 -0500

    Nov 2 2024 12:49am

--- runBackend.bat
+++ runBackend.bat
@@ -1,12 +1,4 @@
 @echo Off
-<<<<<<< HEAD
 cd D:\POS\ThePostSouvernir\backEnd
-=======
-<<<<<<< HEAD
-cd D:\POS\ThePostSouvernir\backEnd
-=======
-cd D:\POS\ThePostSouvernir\backEnd\
->>>>>>> 8f9e8d6fc6294db6c0f265e7e838f05c0f039e7f
->>>>>>> 275c0d2d85015403b1010e2c849652cef39e3bd2
 node app.js
 
